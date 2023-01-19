eaa8867b6c41f808cb6efa8eb6b930bbaa9d7bba
Nicholas Lysons <120535806+NicholasLysons@users.noreply.github.com>
2023-01-19 15:37:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var generation = document.querySelector("#generate");
+
+function writePassword() {
+  var password = generatePassword();
+  
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
